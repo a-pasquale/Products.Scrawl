d3ca907bee3be658f624a8d3bea2fa0ddba72eed
jbaldivieso <jbaldivieso@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-10-23 00:20:39 +0000

    Moving trunk to an egg
    git-svn-id: http://svn.plone.org/svn/collective/Scrawl/trunk@74241 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='Products.Scrawl',
+      version=version,
+      description="Scrawl is a dirt-simple blog product for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='jbaldivieso',
+      author_email='jonb@onenw.org',
+      url='http://plone.org/products/scrawl',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
