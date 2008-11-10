fa5f3a3d03aa882523ff8496296f83f1e93104f7
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-11-10 19:31:40 +0000

    tweak trove classifiers
    git-svn-id: http://svn.plone.org/svn/collective/Scrawl/trunk@75414 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(name='Products.Scrawl',
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
         ],
       keywords='',
       author='jbaldivieso',
